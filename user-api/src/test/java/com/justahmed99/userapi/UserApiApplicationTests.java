db3710c8e591f341c588c7a8e8a2ffd44bec4c4b
Ahmad Mujahid <ahmad.kirghizia@gmail.com>
2023-05-16 18:26:33 +0700

    first commit

---
+++ user-api/src/test/java/com/justahmed99/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justahmed99.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ worker/src/test/java/com/justahmed99/worker/WorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justahmed99.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
